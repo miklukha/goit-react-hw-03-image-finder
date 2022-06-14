cc252e0ba7837f918d17528fc59dae9d6dabc7fa
Anna Miklukha <miklukha.anna@ukr.net>
2022-06-14 19:02:59 +0200

    remove import Component in ImageGalleryItem

--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Item, Image } from './ImageGalleryItem.styled';
 export function ImageGalleryItem({ image, tags, onClick, largeImg }) {
